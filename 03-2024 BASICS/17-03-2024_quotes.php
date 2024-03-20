6011a118673182d9c34253a6d3175c12aeffc373
Adrianna Turkowiak <adrianna.turkowiak.programista@gmail.com>
2024-03-20 20:10:32 +0100

    adding quotes and variable interpolation

---
+++ 03-2024 BASICS/17-03-2024_quotes.php
@@ -0,0 +1,10 @@
+<?php
+
+$firstDate = "3 o'clock";
+$secondDate = '4 o\' clock';
+
+$nicDwaRazy = "Nic dwa razy się nie zdarza \nI nie zdarzy, z tej przyczyny \nZrodziliśmy się bez wprawy \nI pomrzemy bez rutyny";
+
+echo $nicDwaRazy;
+
+?>
