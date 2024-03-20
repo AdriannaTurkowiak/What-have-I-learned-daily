6fced844bfe98f46d4e2fcc65b94b38f0989b1ab
Adrianna Turkowiak <adrianna.turkowiak.programista@gmail.com>
2024-03-20 22:11:19 +0100

    Adding array lesson

---
+++ 03-2024 BASICS/20-03-2024_arrays.php
@@ -0,0 +1,16 @@
+<?php
+
+$wpisyNaBlogu = ["pierwszy" => "Kiedy będzie koniec świata", 
+                "drugi" => "Gdzie są pieniądze z Komunii", 
+                "trzeci" => "Czy dobrze zrobiłem, że się ożeniłem"];
+
+echo $wpisyNaBlogu["drugi"];
+
+
+$imie = "Ada";
+$nazwisko = "Turkowiak";
+$daneOsobowe = [$imie, $nazwisko];
+
+var_dump($daneOsobowe);
+
+?>
